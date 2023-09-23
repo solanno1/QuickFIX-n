11c7903e44f1a4136cea1c723564d6736b307638
SOLANO <solano.gordoni@g10x.com.br>
2023-09-23 19:02:14 -0300

    Add project files.

---
+++ OrderAccumulator/Program.cs
@@ -0,0 +1,90 @@
+﻿using QuickFix;
+using QuickFix.Fields;
+using QuickFix.FIX42;
+using System;
+using System.Collections.Generic;
+using QuickFix.Transport;
+
+namespace OrderAccumulator
+{
+    // https://new.quickfixn.org/n/documentation/
+    class Program : MessageCracker, IApplication
+    {        
+        private readonly Dictionary<string, decimal> exposureBySymbol = new Dictionary<string, decimal>();
+        private const decimal LIMIT = 1000000M;
+
+        //Métodos da interface IApplication (nao estao fazendo nada, mas fui obrigado a colocar pois a interface exige).
+        public void OnCreate(SessionID sessionID) { }
+        public void OnLogon(SessionID sessionID) { }
+        public void OnLogout(SessionID sessionID) { }
+        public void ToAdmin(QuickFix.Message message, SessionID sessionID) { }
+        public void FromAdmin(QuickFix.Message message, SessionID sessionID) { }
+        public void ToApp(QuickFix.Message message, SessionID sessionID) { }
+
+        //Implementação que chama a função crack que processa as mensagens FIX.
+        public void FromApp(QuickFix.Message message, SessionID sessionID)
+        {
+            Crack(message, sessionID);
+        }
+
+        //Implementação para quando uma mensagens NewOrderSingle é recebida.
+        public void OnMessage(NewOrderSingle order, SessionID sessionID)
+        {
+            //Extração de informações.
+            string symbol = order.Symbol.Obj;
+            decimal price = order.Price.Obj;
+            decimal qty = order.OrderQty.Obj;
+            char side = order.Side.Obj;
+
+            //Cálculo de acordo com o exercício proposto.
+            decimal newExposure = price * qty * (side == Side.BUY ? 1 : -1);
+
+            //Verificação para ver se a exposição atual por simbolo já existe.
+            if (exposureBySymbol.ContainsKey(symbol))
+            {
+                newExposure += exposureBySymbol[symbol];
+            }
+
+            //Verificar se passa do limite pré-definido.
+            if (Math.Abs(newExposure) > LIMIT)
+            {
+                OrderCancelReject reject = new OrderCancelReject(
+                 new OrderID("MissingOrderID"),  
+                 new ClOrdID(order.ClOrdID.Obj),
+                 new OrigClOrdID(order.ClOrdID.Obj),
+                 new OrdStatus(OrdStatus.REJECTED),
+                 new CxlRejResponseTo(CxlRejResponseTo.ORDER_CANCEL_REQUEST)
+            );
+                Session.SendToTarget(reject, sessionID);
+            }
+            else
+            {
+                exposureBySymbol[symbol] = newExposure;
+                ExecutionReport report = new ExecutionReport(
+                new OrderID("OrderID"),
+                new ExecID("ExecID"),
+                new ExecTransType(ExecTransType.NEW),
+                new ExecType(ExecType.NEW),
+                new OrdStatus(OrdStatus.NEW),
+                new Symbol(symbol), 
+                new Side(side), 
+                new LeavesQty(0), 
+                new CumQty(0), 
+                new AvgPx(0.0m)
+                );
+                Session.SendToTarget(report, sessionID);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            SessionSettings settings = new SessionSettings(@"C:\\Users\\SOLANO\\source\\repos\\OrderGenerator\\OrderAccumulator\\quickfix-accumulator.cfg");
+            IApplication app = new Program();
+            IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+            ILogFactory logFactory = new FileLogFactory(settings);
+            ThreadedSocketAcceptor acceptor = new ThreadedSocketAcceptor(app, storeFactory, settings, logFactory);
+
+            acceptor.Start();
+        }
+    }
+}
---
+++ OrderGenerator/Program.cs
@@ -0,0 +1,85 @@
+﻿using QuickFix;
+using QuickFix.Fields;
+using QuickFix.FIX42;
+using QuickFix.Transport;
+using System;
+using System.Threading;
+
+namespace OrderGenerator
+{
+    class Program : MessageCracker, IApplication
+    {
+        private static readonly Random random = new Random();
+
+        //Métodos da interface IApplication (nao estao fazendo nada, mas fui obrigado a colocar pois a interface exige).
+        public void OnCreate(SessionID sessionID) { }
+        public void OnLogon(SessionID sessionID) { }
+        public void OnLogout(SessionID sessionID) { }
+        public void ToAdmin(QuickFix.Message message, SessionID sessionID) { }
+        public void FromAdmin(QuickFix.Message message, SessionID sessionID) { }
+        public void ToApp(QuickFix.Message message, SessionID sessionID) { }
+        public void FromApp(QuickFix.Message message, SessionID sessionID) { }
+
+        //Simbolos das açoes
+        private static string[] Symbols = { "PETR4", "VALE3", "VIIA4" };
+        //Lados das ordens compra/venda
+        private static string[] Sides = { Side.BUY.ToString(), Side.SELL.ToString() };
+
+        static void Main(string[] args)
+        {
+            //Criando uma sessionsettings para configurar a sessão.
+            SessionSettings settings = new SessionSettings(@"C:\\Users\\SOLANO\\source\\repos\\OrderGenerator\\OrderGenerator\\quickfix.cfg");
+
+            //Instancia de program para usar como aplicação fix.
+            IApplication app = new Program();
+
+            //Armazenar mensagens fix em um arquivo.
+            IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+
+            //Armazenar logs em um arquivo
+            ILogFactory logFactory = new FileLogFactory(settings);
+
+            //Iniciar uma sessao fix.
+            SocketInitiator initiator = new SocketInitiator(app, storeFactory, settings, logFactory);
+            initiator.Start();
+            
+            while (true)
+            {
+                Thread.Sleep(1000);
+                GenerateOrder();
+            }
+        }
+
+        public static void GenerateOrder()
+        {
+            try
+            {
+                //Gerar aleatóriamente simbolo, lado de ordem, qtd e preço
+                string symbol = Symbols[random.Next(Symbols.Length)];
+                string side = Sides[random.Next(Sides.Length)];
+                int qty = random.Next(1, 100000);
+                decimal price = Math.Round((decimal)random.NextDouble() * 1000, 2);
+
+                //Campos apropriados para order NewOrderSingle
+                NewOrderSingle order = new NewOrderSingle(
+                    new ClOrdID(DateTime.Now.Ticks.ToString()),
+                    new HandlInst('1'),
+                    new Symbol(symbol),
+                    new Side(side[0]),
+                    new TransactTime(DateTime.UtcNow),
+                    new OrdType(OrdType.LIMIT));
+
+                //Atribuições
+                order.OrderQty = new OrderQty(qty);
+                order.Price = new Price(price);
+
+                //Envio
+                Session.SendToTarget(order, new SessionID("FIX.4.2", "OrderGenerator", "OrderAccumulator"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error generating order: {ex.Message}");
+            }
+        }
+    }
+}
